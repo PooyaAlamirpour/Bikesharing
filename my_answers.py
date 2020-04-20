49185bbac60eabe3c3bab9d03b4c9679465d990f
PAlamirpour <P.Alamirpour@gmail.com>
2020-04-20 22:18:42 +0000

    Run successfully

--- my_answers.py
+++ my_answers.py
@@ -138,6 +138,6 @@ class NeuralNetwork(object):
 # Set your hyperparameters here
 ##########################################################
 iterations = 4000
-learning_rate = 0.8
+learning_rate = 1
 hidden_nodes = 16
 output_nodes = 1
